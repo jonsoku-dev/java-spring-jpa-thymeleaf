92a0236be9ae84fce91640e47a5cfec33552b366
jongseok-lee <jongseok.lee@linecorp.com>
2020-12-08 00:39:46 +0900

    Item repository 개발

---
+++ src/main/java/jpabook/jpashop/repository/ItemRepository.java
@@ -0,0 +1,34 @@
+package jpabook.jpashop.repository;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+
+import org.springframework.stereotype.Repository;
+
+import jpabook.jpashop.domain.item.Item;
+
+@Repository
+public class ItemRepository {
+
+    private final EntityManager em;
+
+    public ItemRepository(EntityManager em) {this.em = em;}
+
+    public void save(Item item) {
+        if (item.getId() == null) {
+            em.persist(item);
+        } else {
+            em.merge(item);
+        }
+    }
+
+    public Item findOne(Long id) {
+        return em.find(Item.class, id);
+    }
+
+    public List<Item> findAll() {
+        return em.createQuery("select i from Item i", Item.class)
+                 .getResultList();
+    }
+}
