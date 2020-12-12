84ec18c00d8d576c31c92039092d8d25a89f9c5f
jonsoku2 <whdtjr2792@gmail.com>
2020-12-12 15:03:32 +0900

    주문 조회 V3: 엔티티를 DTO로 변환 - 페치조인 최적화 - 아직도 사용하면 안됨

---
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,103 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import lombok.Data;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1() {
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+
+    /**
+     * 지연로딩으로 인해 SQL 쿼리문이 N + 1 날라간다... 문제가있음 !
+     * List개수만큼 쿼리가나감..... 망!!!
+     */
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> ordersV2() {
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    /**
+     * Dto만들 때 주의사항 ! Dto로 반환한다해도 정작 Dto 안에 Entity가 존재하면 안된다.
+     * 즉, Dto안에서 엔티티가 있다면 이것 또한 Dto로 반환해야함 !
+     * 엔티티를 노출하면 엔티티가 수정될때 화면단에도 영향이 크므로, 엔티티가 아닌 화면전용 Dto를 만드는 것이 현명한 방법임.
+     */
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order order) {
+            orderId = order.getId();
+            name = order.getMember().getName();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+            // 프록시 초기화
+            // Dto안에 entity가 존재하면 안된다 !
+//            order.getOrderItems().stream().forEach(oi -> oi.getItem().getName());
+//            orderItems = order.getOrderItems();
+            // 엔티티와 화면의 의존성을 끊어야한다. 그러니 내부도 dto로 변환
+            orderItems = order.getOrderItems().stream()
+                    .map(orderItem -> new OrderItemDto(orderItem))
+                    .collect(Collectors.toList());
+        }
+    }
+
+    // order와 orderItems를 join하면 데이터가 뻥튀기가된다. (중복데이터 발생)
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3() {
+        List<Order> orders = orderRepository.findAllWithItem();
+        List<OrderDto> collect = orders.stream()
+                .map(o -> new OrderDto(o))
+                .collect(Collectors.toList());
+        return collect;
+    }
+
+    @Getter
+    static class OrderItemDto {
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getCount();
+        }
+    }
+}
--- src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -53,7 +53,7 @@ public class OrderRepository {
             jpql += " m.name like :name";
         }
         TypedQuery<Order> query = em.createQuery(jpql, Order.class)
-                                    .setMaxResults(1000); // 최대 1000건
+                .setMaxResults(1000); // 최대 1000건
         if (orderSearch.getOrderStatus() != null) {
             query = query.setParameter("status", orderSearch.getOrderStatus());
         }
@@ -61,5 +61,28 @@ public class OrderRepository {
             query = query.setParameter("name", orderSearch.getMemberName());
         }
         return query.getResultList();
+    }
+
+    // order와 orderItems를 join하면 데이터가 뻥튀기가된다. (중복데이터 발생)
+    // -> distinct를 사용한다. 그런데 이건 DB Query에서는 중복제거가 적용이 되지않는다.
+    // -> 애플리케이션에 올라와야 중복이 제거된다. (디비에서 데이터를 받고 jpa가 중복을 제거한다는 의미)
+    // -> 하지만 이 방법을 사용하면 페이지네이션이 불가능하다 !!
+    // -> 디비에서 중복데이터가 발생하기때문에 데이터양에 대한 신뢰도가 현저히 떨어지기때문에
+    public List<Order> findAllWithItem() {
+        return em.createQuery(
+                "select distinct o from Order o" +
+                        " join fetch o.member m" +
+                        " join fetch o.delivery d" +
+                        " join fetch o.orderItems oi" +
+                        " join fetch oi.item i", Order.class)
+                // limit, offset이 없고, hibernate가 warn을 띄운다.
+                // 2020-12-12 15:01:09.359  WARN 50454 --- [nio-8083-exec-1] o.h.h.internal.ast.QueryTranslatorImpl
+                // : HHH000104: firstResult/maxResults specified with collection fetch; applying in memory!
+                // 단점1: 데이터가 뻥튀기된다.
+                // 단점2: 메모리에서 페이징처리를 한다. 데이터가 만개가있으면 만개를 어플리케이션에 퍼올린다음 소팅한다.
+                // 이럴경우 쿼리 몇개만들어오면 서버가 작살난다.
+//                .setFirstResult(1)
+//                .setMaxResults(1000)
+                .getResultList();
     }
 }
