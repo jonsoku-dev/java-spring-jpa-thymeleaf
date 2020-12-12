1fe422c9075f6eafb4941ce15203b52b9ea5d5c5
jonsoku2 <whdtjr2792@gmail.com>
2020-12-13 08:35:23 +0900

    주문 조회 V5: JPA에서 DTO 직접 조회 - 컬렉션 조회최적화 - 데이터쿼리문이 줄어드는 대신, 코드가 길어짐.

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -110,6 +110,11 @@ public class OrderApiController {
         return orderQueryRepository.findOrderQueryDtos();
     }
 
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5() {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
     @Getter
     static class OrderItemDto {
         private String itemName;
